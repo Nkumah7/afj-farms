da0e3118701cefd8554c03e528c022fb34628001
Jo <josephnkumah97@gmail.com>
2021-07-15 08:59:08 +0100

    Product page addons

--- js/main.js
+++ js/main.js
@@ -142,6 +142,22 @@
 	};
 	scrollWindow();
 
+	// Back to top button
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 100) {
+		  $('.back-to-top').fadeIn('slow');
+		} else {
+		  $('.back-to-top').fadeOut('slow');
+		}
+	  });
+	
+	  $('.back-to-top').click(function() {
+		$('html, body').animate({
+		  scrollTop: 0
+		}, 1500, 'easeInOutExpo');
+		return false;
+	  });
+
 	var counter = function() {
 		
 		$('#section-counter, .wrap-about, .ftco-counter').waypoint( function( direction ) {
